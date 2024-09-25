9031380936ed07d246b22b7ed6353ce2faf010d3
sarkarmamun171 <sarkar.pau.cse171@gmail.com>
2024-09-25 10:00:31 +0600

    Sales updated

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -33,11 +33,48 @@
                                 <input type="text" class="form-control" placeholder="Total Amount">
                             </td>
                             <td>
-                                <input type="text" class="form-control">
+                                <button class="btn btn-success" type="button" onclick="addproduct()">Add</button>
                             </td>
                         </tr>
                     </table>
               </form>
+              <table class="table table-bordered" id="product_list">
+                <caption>Product</caption>
+                <thead>
+                    <tr>
+                        <th style="width:40px">Remove</th>
+                        <th>Product Name</th>
+                        <th>Product Code</th>
+                        <th>Unit Price</th>
+                        <th>Quantity</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+            <div class="col-sm-4">
+                <div class="col-s12 m6 offset m4">
+                    <div class="form-group" align="left">
+                        <label for="form-label">Total</label>
+                        <input type="text" class="form-control" id="total" name="total" size="30px" required >
+                    </div>
+                    <div class="form-group" align="left">
+                        <label for="form-label">Pay</label>
+                        <input type="text" class="form-control" id="pay" name="pay" size="30px" required >
+                    </div>
+                    <div class="form-group" align="left">
+                        <label for="form-label">Balance</label>
+                        <input type="text" class="form-control" id="balance" name="balance" size="30px" required >
+                    </div>
+                    <div class="form-group" align="left">
+                        <label for="form-label">Due</label>
+                        <input type="text" class="form-control" id="due" name="due" size="30px" required >
+                    </div>
+                    <div class="form-group" align="left">
+                        <button type="submit" class="btn btn-primary">Update Invoice</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
